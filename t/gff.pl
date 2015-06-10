7eed614dd974c2e2ef2257e3a88ce8ea305227e7
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2015-06-10 15:06:48 +0200

    test ->next_group()

--- t/gff.pl
+++ t/gff.pl
@@ -8,12 +8,20 @@ use Gff::Feature;
 use Data::Dumper;
 $Data::Dumper::Sortkeys = 1;
 
-my $gp = Gff::Parser->new();
-#$gp->is(sub{
-#             my $feat = $_[0];
-#             return $feat->type eq 'gene' && $feat->strand eq '-';
-#         });
+my $gp = Gff::Parser->new(
+    @ARGV ? (file => $ARGV[0]) : (),
+    # region => {
+    #     id => "MaV-gen-2.0",
+    #     from => 6000,
+    #     to => 8000,
+    # }
+);
 
-while (my $f = $gp->next_feature) {
-    print Dumper $f; last;
+$gp->add_condition(sub{$_[0]->type eq 'gene'});
+
+my $fh = $gp->fh;
+
+while (my $g = $gp->next_groups) {
+    print Dumper([keys $g->{groups}]);
 }
+
